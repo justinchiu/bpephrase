81a5d0e1d20d1031dea191caa6b7fe2886c6da86
Justin Chiu <justc@seas.upenn.edu>
2017-10-08 15:46:33 -0400

    staaarting

--- src/main.cc
+++ src/main.cc
@@ -2,9 +2,18 @@
 #include <iostream>
 #include <string>
 
+#include "bpephrase/options.h"
+
+using namespace bpephrase;
+
 int buffer_size = 65536;
 
-int main() {
+int main(int argc, char** argv) {
+    cxxopts::Options options = initOptions();
+    options.parse(argc, argv);
     auto endl = "\n";
-    std::cout << "hello" << endl;
+    std::cout << "hello!" << endl;
+
+    auto input = options["input"].as<std::string>();
+    std::cout << input << endl;
 }
