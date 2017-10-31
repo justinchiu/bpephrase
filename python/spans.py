51002742909ef486fb60c926f7c64e51165add64
Justin Chiu <justinchiu@g.harvard.edu>
2017-10-31 16:19:42 -0400

    finish spans

--- python/spans.py
+++ python/spans.py
@@ -18,7 +18,8 @@ def enumerative(seq, n, value_fn):
         for tail, tail_val in enumerative(seq[i+n:], n, value_fn):
             yield (chunk + tail, chunk_val + tail_val)
 
-def best_span(seq, n, value_fn):
+def best_spans(seq, n, value_fn):
+    debug = False
     # Simple DP for getting the best spans while ignorning spans that
     # have a value of 0.
     history = np.zeros((n, 2))
@@ -42,6 +43,11 @@ def best_span(seq, n, value_fn):
             backpointers[0,0] = \
                 backpointers[0,0] + [chunk] if head[0] > head[1] \
                 else backpointers[0,1] + [chunk]
+        else:
+            # We do not want to add chunks that have value 0
+            backpointers[0,0] = \
+                backpointers[0,0] if head[0] > head[1] \
+                else backpointers[0,1]
         if backpointers[1:][overlap_idx]:
             backpointers[0,1] = backpointers[1:][overlap_idx]
 
@@ -53,7 +59,8 @@ def best_span(seq, n, value_fn):
         history = np.roll(history, -1, 0)
         i += 1
 
-    return history, backpointers
+    idx = history[-1].argmax()
+    return backpointers[-1, idx], history[-1, idx] 
 
 def get_remainder(seq, spans):
     # Return a list of disjoint segments of the original sequence
@@ -62,34 +69,88 @@ def get_remainder(seq, spans):
     sentence = []
     spans = deepcopy(spans)
     i = 0
+    j = 0
     while i < len(seq):
         word = seq[i]
-        if spans[0] != word:
+        if not spans:
+            new_sentences.append(seq[i:])
+            break
+        elif spans[0][0] != word:
             sentence.append(word)
             i += 1
-        else sentence:
-            new_sentences.append(sentence)
-            sentence = []
+        elif spans[0][0] == word:
+            if sentence:
+                new_sentences.append(sentence)
+                sentence = []
             i += len(spans[0])
             spans.pop(0)
+    if sentence:
+        new_sentences.append(sentence)
     return new_sentences
 
+def concatmap(f, xs):
+    acc = []
+    for x in xs:
+        acc.extend(f(x))
+    return acc
+
+def gen_flatten(xs):
+    if not isinstance(xs, (list, tuple)):
+        yield xs
+    else:
+        for x in xs:
+            if isinstance(x, (list,tuple)):
+                for y in gen_flatten(x):
+                    yield y
+            else:
+                yield x
+
+def get_parse(seq, ns, value_fn):
+    all_spans = []
+    indices = list(range(len(seq)))
+    value_fn_idxs = lambda idxs: value_fn(tuple(map(lambda x: seq[x], idxs)))
+    remainder = [indices]
+    for n in sorted(ns, reverse=True):
+        spans = concatmap(lambda x: best_spans(x, n, value_fn_idxs)[0], remainder)
+        all_spans.extend(spans)
+        all_spans.sort(key=lambda x: x[0])
+        remainder = get_remainder(indices, all_spans)
+        print(all_spans, remainder)
+    unigrams = map(lambda x: (x,), gen_flatten(remainder))
+    all_spans.extend(unigrams)
+    all_spans.sort(key=lambda x:x[0])
+    return map(lambda x: "_".join(map(lambda idx: seq[idx], x)), all_spans)
 
 if __name__ == "__main__":
     print("Testing stuff")
     n = 3
-    seq = list(range(9))
+    seq = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+           "o", "p", "q", "r", "s", "t", "u", "v"]
 
     value_fn = Counter({
-        (0,1,2): 1,
-        (2,3,4): 2,
-        (4,5,6): 10,
-        (6,7,8): 3,
+        (seq[0], seq[1], seq[2]): 1,
+        (seq[2], seq[3], seq[4]): 2,
+        (seq[4], seq[5], seq[6]): 10,
+        (seq[6], seq[7], seq[8]): 3,
+        (seq[15], seq[16], seq[17]): 3,
     })
+    for i in range(len(seq)-1):
+        value_fn[(seq[i], seq[i+1])] += 1
 
-    print(brute_force(seq, n, lambda x: 1))
-    print(brute_force(seq, n, lambda x: value_fn[x]))
+    #print(brute_force(seq, n, lambda x: 1))
+    #print(brute_force(seq, n, lambda x: value_fn[x]))
 
-    print(best_span(seq, n, lambda x: 1))
-    print(best_span(seq, n, lambda x: value_fn[x]))
+    #print(best_spans(seq, n, lambda x: 1))
+    #print(best_spans(seq, n, lambda x: value_fn[x]))
+    #spans = best_spans(seq, n, lambda x: value_fn[x])
+    #remainder = get_remainder(seq, spans[0])
+    #print(concatmap(lambda x: [x], [1,2,3,4,5]))
+    #ok = concatmap(lambda x: best_spans(x, n-1, lambda x: 1)[0], remainder)
+    #print(spans[0] + ok)
+    #lol = get_remainder(seq, sorted(spans[0] + ok, key=lambda x: x[0]))
+    #print(lol)
+    #parse = get_parse(seq, [2,3], lambda x: value_fn[x])
+    #print(parse)
+    parse = get_parse(seq, [2,3], lambda x: 1 if x in value_fn else 0)
+    print(list(parse))
 
