cc99f68728e208fc846204180ece618790257fb2
Justin Chiu <justinchiu@g.harvard.edu>
2018-02-19 15:20:44 -0500

    memory problems?

--- scripts/wmt14deen.sh
+++ scripts/wmt14deen.sh
@@ -1,49 +1,50 @@
-ROOT=/n/rush_lab/data/wmt14-de-en
-TEXT=${ROOT}/data/wmt14_en_de
-DISTILL=${ROOT}/data/wmt14_en_de.distill
-PHRASE=${ROOT}/data/wmt14_en_de.phrase
+WMT_ROOT=/n/rush_lab/data/wmt14-de-en
+WMT_TEXT=${WMT_ROOT}/data/wmt14_en_de
+WMT_PHRASE_DIR=${WMT_ROOT}/phrasedata
+WMT_DISTILL=${WMT_PHRASE_DIR}/wmt14_en_de.distill
+WMT_PHRASE=${WMT_PHRASE_DIR}/wmt14_en_de.phrase
 
-SRC_TRAIN=${TEXT}/train.de
-SRC_TRAIN_OUT=${PHRASE}/train.phrase.de
-SRC_TRAIN_REPEAT_OUT=${PHRASE}/train.phrase.repeat.de
+WMT_SRC_TRAIN=${WMT_TEXT}/train.de
+WMT_SRC_TRAIN_OUT=${WMT_PHRASE}/train.phrase.de
+WMT_SRC_TRAIN_REPEAT_OUT=${WMT_PHRASE}/train.phrase.repeat.de
 # NOT YET DONE
-TGT_TRAIN_MACHINE=${DISTILL}/train.en.baseline.brnn.out
-TGT_TRAIN=${TEXT}/train.en
-TGT_TRAIN_OUT=${PHRASE}/train.phrase.en
-TGT_TRAIN_OUT_NODISTILL=${PHRASE}/train.phrase.nodistill.en
+WMT_TGT_TRAIN_MACHINE=${WMT_DISTILL}/train.en.baseline.brnn.out
+WMT_TGT_TRAIN=${WMT_TEXT}/train.en
+WMT_TGT_TRAIN_OUT=${WMT_PHRASE}/train.phrase.en
+WMT_TGT_TRAIN_OUT_NODISTILL=${WMT_PHRASE}/train.phrase.nodistill.en
 
-SRC_VALID=${TEXT}/valid.de
-SRC_VALID_OUT=${PHRASE}/valid.phrase.de
-SRC_VALID_REPEAT_OUT=${PHRASE}/valid.phrase.repeat.de
-TGT_VALID=${TEXT}/valid.en
-TGT_VALID_OUT=${PHRASE}/valid.phrase.en
+WMT_SRC_VALID=${WMT_TEXT}/valid.de
+WMT_SRC_VALID_OUT=${WMT_PHRASE}/valid.phrase.de
+WMT_SRC_VALID_REPEAT_OUT=${WMT_PHRASE}/valid.phrase.repeat.de
+WMT_TGT_VALID=${WMT_TEXT}/valid.en
+WMT_TGT_VALID_OUT=${WMT_PHRASE}/valid.phrase.en
 
-SRC_TEST=${TEXT}/test.de
-SRC_TEST_OUT=${PHRASE}/test.phrase.de
-SRC_TEST_REPEAT_OUT=${PHRASE}/test.phrase.repeat.de
-TGT_TEST=${TEXT}/test.en
-TGT_TEST_OUT=${PHRASE}/test.phrase.en
+WMT_SRC_TEST=${WMT_TEXT}/test.de
+WMT_SRC_TEST_OUT=${WMT_PHRASE}/test.phrase.de
+WMT_SRC_TEST_REPEAT_OUT=${WMT_PHRASE}/test.phrase.repeat.de
+WMT_TGT_TEST=${WMT_TEXT}/test.en
+WMT_TGT_TEST_OUT=${WMT_PHRASE}/test.phrase.en
 
-SRC_SAVE=${ROOT}/phrase.src.pkl
-TGT_SAVE=${ROOT}/phrase.tgt.pkl
+WMT_SRC_SAVE=${WMT_PHRASE_DIR}/phrase.src.pkl
+WMT_TGT_SAVE=${WMT_PHRASE_DIR}/phrase.tgt.pkl
 
-dbg_ngrams_src() {
-    python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_VALID \
-        --fit $SRC_VALID \
+dbg_wmt_ngrams_src() {
+    time python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
+        --corpus $WMT_SRC_VALID \
+        --fit $WMT_SRC_VALID \
         --save src.save.dbg \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10
-    python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_VALID \
+    time python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
+        --corpus $WMT_SRC_VALID \
         --output train.output.src.dbg \
         --load src.save.dbg \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10
-    python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_VALID \
+    time python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
+        --corpus $WMT_SRC_VALID \
         --output train.output.src.repeat.dbg \
         --load src.save.dbg \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
@@ -52,16 +53,16 @@ dbg_ngrams_src() {
         --repeat-ngrams
 }
 
-dbg_ngrams_tgt() {
+dbg_wmt_ngrams_tgt() {
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_TRAIN \
-        --fit $TGT_TRAIN \
+        --corpus $WMT_TGT_TRAIN \
+        --fit $WMT_TGT_TRAIN \
         --save tgt.save.dbg \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_TRAIN_MACHINE \
+        --corpus $WMT_TGT_TRAIN_MACHINE \
         --output train.output.tgt.dbg \
         --load tgt.save.dbg \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
@@ -69,56 +70,56 @@ dbg_ngrams_tgt() {
         --min-occurrence 10
 }
 
-run_ngrams_src() {
+run_wmt_ngrams_src() {
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_TRAIN \
-        --fit $SRC_TRAIN \
-        --save $SRC_SAVE \
+        --corpus $WMT_SRC_TRAIN \
+        --fit $WMT_SRC_TRAIN \
+        --save $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDSRCGEN=$!
     wait $PIDSRCGEN && echo "src gen done!"
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_TRAIN \
-        --output $SRC_TRAIN_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_TRAIN \
+        --output $WMT_SRC_TRAIN_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDSRCTRAIN=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_VALID \
-        --output $SRC_VALID_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_VALID \
+        --output $WMT_SRC_VALID_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDSRCVALID=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_TEST \
-        --output $SRC_TEST_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_TEST \
+        --output $WMT_SRC_TEST_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDSRCTEST=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_TRAIN \
-        --output $SRC_TRAIN_REPEAT_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_TRAIN \
+        --output $WMT_SRC_TRAIN_REPEAT_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --repeat-ngrams \
         --min-occurrence 10 & PIDSRCTRAINREPEAT=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_VALID \
-        --output $SRC_VALID_REPEAT_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_VALID \
+        --output $WMT_SRC_VALID_REPEAT_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --repeat-ngrams \
         --min-occurrence 10 & PIDSRCVALIDREPEAT=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $SRC_TEST \
-        --output $SRC_TEST_REPEAT_OUT \
-        --load $SRC_SAVE \
+        --corpus $WMT_SRC_TEST \
+        --output $WMT_SRC_TEST_REPEAT_OUT \
+        --load $WMT_SRC_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --repeat-ngrams \
@@ -131,34 +132,34 @@ run_ngrams_src() {
     wait $PIDSRCTESTREPEAT && echo "src test repeat done!"
 }
 
-run_ngrams_tgt() {
+run_wmt_ngrams_tgt() {
     echo "Running target ngrams"
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_TRAIN \
-        --fit $TGT_TRAIN \
-        --save $TGT_SAVE \
+        --corpus $WMT_TGT_TRAIN \
+        --fit $WMT_TGT_TRAIN \
+        --save $WMT_TGT_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDTGTGEN=$!
     wait $PIDTGTGEN && echo "tgt train done!"
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_TRAIN \
-        --output $TGT_TRAIN_OUT \
-        --load $TGT_SAVE \
+        --corpus $WMT_TGT_TRAIN \
+        --output $WMT_TGT_TRAIN_OUT \
+        --load $WMT_TGT_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDTGTTRAIN=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_TRAIN \
-        --output $TGT_TRAIN_OUT_NODISTILL \
-        --load $TGT_SAVE \
+        --corpus $WMT_TGT_TRAIN \
+        --output $WMT_TGT_TRAIN_OUT_NODISTILL \
+        --load $WMT_TGT_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDTGTTRAINNODISTILL=$!
     python /n/home13/jchiu/projects/bpephrase/python/ngrams.py \
-        --corpus $TGT_VALID \
-        --output $TGT_VALID_OUT \
-        --load $TGT_SAVE \
+        --corpus $WMT_TGT_VALID \
+        --output $WMT_TGT_VALID_OUT \
+        --load $WMT_TGT_SAVE \
         --ngram-orders 2 3 4 5 6 7 8 9 10 \
         --topks 10000 \
         --min-occurrence 10 & PIDTGTVALID=$!
